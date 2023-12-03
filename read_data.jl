92c814abff9e7084a4d4128ca3091aee634f9efa
James Doss-Gollin <jdossgollin@rice.edu>
2023-12-03 17:43:41 -0600

    Additional bug squashes (1.9.4 issue? TBD?)

--- read_data.jl
+++ read_data.jl
@@ -14,6 +14,7 @@ struct SingleDurationAnnMaxRecord{T<:AbstractFloat}
     state::AbstractString
     lon::T
     lat::T
+    elevation::T
     records::Vector{Observation{T}}
 end
 
@@ -32,7 +33,8 @@ function parse_metadata(line::AbstractString)
     lon_chars = line[65:74]
     lon_chars = split(lon_chars, ",")[1]
     lon = parse(Float64, strip(lon_chars))
-    return stnid, name, state, lat, lon
+    elevation = parse(Float64, strip(line[77:end]))
+    return stnid, name, state, lat, lon, elevation
 end
 
 """Parse the data from a non-metadata line"""
@@ -48,24 +50,24 @@ end
 function read_record(lines::Vector{<:AbstractString})
 
     # Parse metadata
-    stnid, name, state, lat, lon = parse_metadata(lines[1])
+    stnid, name, state, lat, lon, elevation = parse_metadata(lines[1])
 
     # Parse data lines
     observations = [parse_data(line) for line in lines[2:end]]
 
-    return SingleDurationAnnMaxRecord(stnid, name, state, lon, lat, observations)
+    return SingleDurationAnnMaxRecord(stnid, name, state, lon, lat, elevation, observations)
 end
 
 function group_lines(lines)
-    groups = []  # List to store all groups
-    current_group = []  # Current group of lines
+    groups = Vector{String}[]  # List to store all groups
+    current_group = String[]  # Current group of lines
 
     for line in lines
         if isempty(line)
             # When an empty line is encountered, store the current group (if it's not empty) and reset it
             if !isempty(current_group)
                 push!(groups, current_group)
-                current_group = []
+                current_group = String[]
             end
         else
             # Add non-empty lines to the current group
@@ -83,11 +85,10 @@ end
 
 function read_all_records(filename::String)
     # Read the entire file into memory
-    all_lines = readlines(filename)
+    all_lines = readlines(filename)[2:end]
 
     # Split the content into individual records using a regular expression
-    record_strs = split(all_lines, "")
-    record_strs = [s for s in record_strs[2:end] if !isempty(s)]
+    record_strs = group_lines(all_lines)
 
     # Skip the first line and process each record, filtering out empty records
     records = [read_record(r) for r in record_strs]
@@ -106,7 +107,15 @@ function DataFrames.DataFrame(s::SingleDurationAnnMaxRecord)
         stnid=fill(s.stnid, length(s.records)), year=years, date=dates, value=values
     )
 
-    stn_df = DataFrame(; stnid=s.stnid, name=s.name, state=s.state, lon=s.lon, lat=s.lat)
+    stn_df = DataFrame(;
+        stnid=s.stnid,
+        name=s.name,
+        state=s.state,
+        lon=s.lon,
+        lat=s.lat,
+        elevation=s.elevation,
+        nobs=length(s.records),
+    )
 
     return obs_df, stn_df
 end
